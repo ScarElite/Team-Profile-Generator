554be3f6f4fb2c378bd0bf3852cd7cc4f463a8da
Mitchell Armstrong <mra0211@gmail.com>
2022-10-02 12:38:58 -0400

    Added an Engineer and Intern function that prompts the user with questions for the member being added

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const inquirer = require("inquirer");
 const fs = require("path");
-const { choices } = require("yargs");
 
 const teamData = [];
 
@@ -48,11 +47,70 @@ function addTeamMember() {
     .then(function (choice) {
       if (choice.pickMember === "Engineer") {
         console.log("Adding an Engineer");
+        createEngineer();
       } else if (choice.pickMember === "Intern") {
-        console.log("Adding an Intern");
+        createIntern();
       } else if (choice.pickMember === "No more employees") {
-        console.log("Not adding anymore employees");
+        console.log("Your team has been created!");
       }
+    });
+}
+
+function createEngineer() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "engineerName",
+        message: "What is the name of your engineer?",
+      },
+      {
+        type: "input",
+        name: "engineerId",
+        message: "What is your engineer's id?",
+      },
+      {
+        type: "input",
+        name: "engineerEmail",
+        message: "What is your engineer's email?",
+      },
+      {
+        type: "input",
+        name: "engineerGitHub",
+        message: "What is your engineer's GitHub username?",
+      },
+    ])
+    .then(function () {
+      addTeamMember();
+    });
+}
+
+function createIntern() {
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "internName",
+        message: "What is the name of your intern?",
+      },
+      {
+        type: "input",
+        name: "internId",
+        message: "What is your intern's id?",
+      },
+      {
+        type: "input",
+        name: "internEmail",
+        message: "What is your intern's email?",
+      },
+      {
+        type: "input",
+        name: "internSchool",
+        message: "What is your intern's school?",
+      },
+    ])
+    .then(function () {
+      addTeamMember();
     });
 }
 
